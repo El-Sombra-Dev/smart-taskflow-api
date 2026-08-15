90b50d99fc4f67f4f6b372ea977289d278bef6e9
El-Sombra-Dev <zahzouhamine445@gmail.com>
2026-08-15 04:42:48 +0100

    Create __init__.py

---
+++ router/__init__.py
@@ -0,0 +1,8 @@
+"""
+Routers Package
+"""
+from .auth import router as auth_router
+from .tasks import router as tasks_router
+from .categories import router as categories_router
+
+__all__ = ["auth_router", "tasks_router", "categories_router"]
